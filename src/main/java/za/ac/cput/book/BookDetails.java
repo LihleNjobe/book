44401249958abedde1840b935b6a395474752994
Lihle_N <218193882@mycput.ac.za>
2021-05-12 01:43:45 +0200

    modified the class by adding setters and getters to the class

--- src/main/java/za/ac/cput/book/BookDetails.java
+++ src/main/java/za/ac/cput/book/BookDetails.java
@@ -7,9 +7,52 @@ package za.ac.cput.book;
  */
 public class BookDetails {
 
-    public String title;
-    public String author;
-    public String ISBN;
-    public int year;
+
+    private String title;
+    private String author;
+    private String ISBN;
+    private int year;
+
+
+    public BookDetails(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.year = year;
+
+    }
+
+    public void getTitle ()  {
+        System.out.println("Title: " + title);
+    }
+
+    public void getAuthor() {
+        System.out.println("Author: " + author);
+    }
+
+    public void getISBN() {
+        System.out.println("ISBN: " + ISBN);
+    }
+    public void getYear() {
+        System.out.println("Year: " + year);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
+
+
 
